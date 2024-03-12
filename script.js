6afdc2a10ff0d16a2352a0fe8cb3074e80e68c70
Harsh Keshari <134734274+Keshari818@users.noreply.github.com>
2024-03-12 21:39:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+function init(){
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+}
+
+init()
+
+var crsr = document.querySelector(".cursor")
+var main = document.querySelector(".main")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+10+"px"
+    crsr.style.top = dets.y+10+"px"
+})
+
+var tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:".main",
+        start:"top 27%",
+        end:"top 0",
+        scrub:3,
+    }
+})
+
+tl.to(".page1 h1",{
+    x:-100,      
+},"anim")        //anim is a variable which is written so that both the h1 and h2 perform at same time, we can write anything in place of anim.
+
+tl.to(".page1 h2",{
+    x:100,   
+},"anim")
+
+tl.to(".page1 video",{
+    width:"90%"
+},"anim")
+
+var tl2 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:".main",
+        start:"top -115%",
+        end:"top -120%",
+        scrub:3,
+    }
+})
+
+tl2.to(".main",{
+    backgroundColor:"#fff",
+})
+
+var tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:".main",
+        start:"top -500%",
+        end:"top -510",
+        scrub:3,
+    }
+})
+
+tl3.to(".main",{
+    backgroundColor:"#0F0D0D",
+})
+
+var boxes = document.querySelectorAll(".box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        var att =elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height ="370px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att })`
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "20px"
+        crsr.style.height ="20px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none` 
+    })
+})
